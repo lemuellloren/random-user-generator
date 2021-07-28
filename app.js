ccb36a58c999585349e6c6c1a76efaf7f69ef4a6
lemuellloren <lempakz@yahoo.com>
2021-07-28 14:34:10 +0800

    set up random user generator

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ const app = Vue.createApp({
             lastName: 'Dela Cruz',
             email: 'jean@email.com',
             gender: 'male',
-            photo: 'https://randomuser.me/api/portraits/men/10.jpg' 
+            photo: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
+    },
+    methods: {
+      async getUser() {
+            // TODO: Fetch API 
+            const res = await fetch('https://randomuser.me/api');
+            const { results } = await res.json();
+
+            
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.lastName
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.photo = results[0].picture.large
+        },
     }
-});
+})
 
 app.mount('#app')

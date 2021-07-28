e2fd66a3ca21fead4848c491e8b6035fcd5e9552
lemuellloren <lempakz@yahoo.com>
2021-07-28 14:45:25 +0800

    fix vue issue

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const app = Vue.createApp({
+
+const generateUser = {
     data() {
         return {
             firstName: 'Juan',
@@ -7,21 +8,19 @@ const app = Vue.createApp({
             gender: 'male',
             photo: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
-    },
+    }, 
     methods: {
-      async getUser() {
-            // TODO: Fetch API 
-            const res = await fetch('https://randomuser.me/api');
-            const { results } = await res.json();
-
-            
-            this.firstName = results[0].name.first
-            this.lastName = results[0].name.lastName
-            this.email = results[0].email
-            this.gender = results[0].gender
-            this.photo = results[0].picture.large
-        },
-    }
-})
-
-app.mount('#app')
+        async getUser() {
+              // TODO: Fetch API 
+              const res = await fetch('https://randomuser.me/api');
+              const { results } = await res.json();
+  
+              this.firstName = results[0].name.first
+              this.lastName = results[0].name.lastName
+              this.email = results[0].email
+              this.gender = results[0].gender
+              this.photo = results[0].picture.large
+          },
+      }
+}
+Vue.createApp(generateUser).mount('#app')
